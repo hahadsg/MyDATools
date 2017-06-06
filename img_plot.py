77ad70b89509651002ce2c4a90a900617d48db61
hahadsg <809428197@qq.com>
2017-06-06 07:20:25 +0000

    增加img_plot

---
+++ img_plot.py
@@ -0,0 +1,23 @@
+import keras
+
+def img_to_plot(img):
+    if keras.backend.image_dim_ordering() == 'tf':
+        return np.rollaxis(img, 0, 1).astype(np.uint8)
+    else:
+        return np.rollaxis(img, 0, 3).astype(np.uint8)
+
+def img_plot(img):
+    plt.imshow(to_plot(img))
+
+def img_plots(ims, figsize=(12,6), rows=1, interp=False, titles=None):
+    if type(ims[0]) is np.ndarray:
+        ims = np.array(ims).astype(np.uint8)
+        if (ims.shape[-1] != 3):
+            ims = ims.transpose((0,2,3,1))
+    f = plt.figure(figsize=figsize)
+    for i in range(len(ims)):
+        sp = f.add_subplot(rows, len(ims)//rows, i+1)
+        sp.axis('Off')
+        if titles is not None:
+            sp.set_title(titles[i], fontsize=16)
+        plt.imshow(ims[i], interpolation=None if interp else 'none')
