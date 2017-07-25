d0520ed857cc87609d5a21ecbee594dcd31da104
zhanjiefan <809428197@qq.com>
2017-07-25 16:11:24 +0800

    增加plot.plot_multiclass_feature_dist

--- plot.py
+++ plot.py
@@ -14,101 +14,6 @@ from sklearn.metrics import average_precision_score
 from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
 
-# --------------------------------------------------------------------------------
-# cv结果展示
-# --------------------------------------------------------------------------------
-def plot_grid_search_result(gs_model, x=None, y=None, xfunc=None, yfunc=None):
-    """ 将cv的结果展示
-    如果是一个参数则显示为曲线图，如果是两个参数则显示为heatmap
-
-    Args:
-        gs_model: sklearn.model_selection._search.GridSearchCV
-            进行GridSearchCV的返回值
-        x, y: str, default None
-            图的横纵坐标轴是什么
-        xfunc, yfunc: func, default None
-            是否对横纵坐标的值做处理（有可能需要对x进行log操作）
-
-    """
-    n_paras = len(gs_model.cv_results_['params'])
-    paras_type = list(gs_model.cv_results_['params'][0].keys())
-    n_paras_type = len(paras_type)
-
-    # x为空就赋值为第一个paras_type
-    if x is None: 
-        x = paras_type[0]
-    # y为空，且有两个paras，就赋值为第二个paras_type
-    if y is None and n_paras_type == 2: 
-        y = paras_type[1]
-
-    # 对坐标轴的值进行转换
-    score_trans = lambda score, func: str(func(score) if func is not None else score)
-
-    # print最优参数
-    print('Best params:', gs_model.best_params_)
-    print('Best score:', gs_model.best_score_)
-
-    # plot过程（超过两个轴就不plot）
-    # 一个轴
-    if n_paras_type == 1:
-        # 获取各参数值score
-        cv_score = [
-            [
-                gs_model.cv_results_['mean_test_score'][i],
-                score_trans(gs_model.cv_results_['params'][i][x], xfunc),
-            ] for i in range(n_paras)
-        ]
-        # df
-        cv_score_df = pd.Series(*tuple(zip(*cv_score)))
-        # 改变轴显示值的顺序
-        x_values = [score_trans(v, xfunc) for v in gs_model.get_params()['param_grid'][x]]
-        cv_score_df = cv_score_df[x_values]
-        # plot
-        cv_score_df.plot()
-
-    # 两个轴
-    elif n_paras_type == 2:
-        # 获取各参数值score
-        cv_score = [
-            [
-                gs_model.cv_results_['mean_test_score'][i],
-                score_trans(gs_model.cv_results_['params'][i][x], xfunc),
-                score_trans(gs_model.cv_results_['params'][i][y], yfunc),
-            ] for i in range(n_paras)
-        ]
-        # 将score变成二维的形式
-        cv_score_df = pd.DataFrame(cv_score, columns=['score']+paras_type)
-        cv_score_df = cv_score_df.pivot(y, x, 'score')
-        # 改变轴显示值的顺序
-        x_values = [score_trans(v, xfunc) for v in gs_model.get_params()['param_grid'][x]]
-        y_values = [score_trans(v, yfunc) for v in gs_model.get_params()['param_grid'][y]]
-        cv_score_df = cv_score_df.loc[y_values, x_values]
-        # plot
-        sns.heatmap(cv_score_df, annot=True)
-
-
-# --------------------------------------------------------------------------------
-# 绘制学习曲线
-# --------------------------------------------------------------------------------
-def plot_learning_curve(param_range, train_scores, test_scores):
-    train_scores_mean = np.mean(train_scores, axis=1)
-    train_scores_std = np.std(train_scores, axis=1)
-    test_scores_mean = np.mean(test_scores, axis=1)
-    test_scores_std = np.std(test_scores, axis=1)
-
-    plt.fill_between(param_range, train_scores_mean - train_scores_std,
-                     train_scores_mean + train_scores_std, alpha=0.2,
-                     color="r")
-    plt.fill_between(param_range, test_scores_mean - test_scores_std,
-                     test_scores_mean + test_scores_std, alpha=0.2,
-                     color="g")
-    plt.plot(param_range, train_scores_mean, 'o-', color="r",
-             label="Training score")
-    plt.plot(param_range, test_scores_mean, 'o-', color="g",
-             label="Cross-validation score")
-    plt.legend(loc="best")
-    plt.show()
-
 
 # --------------------------------------------------------------------------------
 # 雷达图
@@ -197,9 +102,6 @@ def radar_factory(num_vars, frame='circle'):
     register_projection(RadarAxes)
     return theta
 
-# --------------------------------------------------------------------------------
-# 绘制雷达图
-# --------------------------------------------------------------------------------
 def radar_plot(data, varlabels=None, legends=None, ax=None, class_size=None):
     """ 绘制雷达图
 
@@ -270,7 +172,103 @@ def plot_precision_recall_curve(y_true, y_proba):
 
 
 # --------------------------------------------------------------------------------
-# 将数据降维 以2D的形式展现
+# 绘制学习曲线
+# --------------------------------------------------------------------------------
+def plot_learning_curve(param_range, train_scores, test_scores):
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+
+    plt.fill_between(param_range, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.2,
+                     color="r")
+    plt.fill_between(param_range, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.2,
+                     color="g")
+    plt.plot(param_range, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(param_range, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+    plt.legend(loc="best")
+    plt.show()
+
+
+# --------------------------------------------------------------------------------
+# cv结果展示
+# --------------------------------------------------------------------------------
+def plot_grid_search_result(gs_model, x=None, y=None, xfunc=None, yfunc=None):
+    """ 将cv的结果展示
+    如果是一个参数则显示为曲线图，如果是两个参数则显示为heatmap
+
+    Args:
+        gs_model: sklearn.model_selection._search.GridSearchCV
+            进行GridSearchCV的返回值
+        x, y: str, default None
+            图的横纵坐标轴是什么
+        xfunc, yfunc: func, default None
+            是否对横纵坐标的值做处理（有可能需要对x进行log操作）
+
+    """
+    n_paras = len(gs_model.cv_results_['params'])
+    paras_type = list(gs_model.cv_results_['params'][0].keys())
+    n_paras_type = len(paras_type)
+
+    # x为空就赋值为第一个paras_type
+    if x is None: 
+        x = paras_type[0]
+    # y为空，且有两个paras，就赋值为第二个paras_type
+    if y is None and n_paras_type == 2: 
+        y = paras_type[1]
+
+    # 对坐标轴的值进行转换
+    score_trans = lambda score, func: str(func(score) if func is not None else score)
+
+    # print最优参数
+    print('Best params:', gs_model.best_params_)
+    print('Best score:', gs_model.best_score_)
+
+    # plot过程（超过两个轴就不plot）
+    # 一个轴
+    if n_paras_type == 1:
+        # 获取各参数值score
+        cv_score = [
+            [
+                gs_model.cv_results_['mean_test_score'][i],
+                score_trans(gs_model.cv_results_['params'][i][x], xfunc),
+            ] for i in range(n_paras)
+        ]
+        # df
+        cv_score_df = pd.Series(*tuple(zip(*cv_score)))
+        # 改变轴显示值的顺序
+        x_values = [score_trans(v, xfunc) for v in gs_model.get_params()['param_grid'][x]]
+        cv_score_df = cv_score_df[x_values]
+        # plot
+        cv_score_df.plot()
+
+    # 两个轴
+    elif n_paras_type == 2:
+        # 获取各参数值score
+        cv_score = [
+            [
+                gs_model.cv_results_['mean_test_score'][i],
+                score_trans(gs_model.cv_results_['params'][i][x], xfunc),
+                score_trans(gs_model.cv_results_['params'][i][y], yfunc),
+            ] for i in range(n_paras)
+        ]
+        # 将score变成二维的形式
+        cv_score_df = pd.DataFrame(cv_score, columns=['score']+paras_type)
+        cv_score_df = cv_score_df.pivot(y, x, 'score')
+        # 改变轴显示值的顺序
+        x_values = [score_trans(v, xfunc) for v in gs_model.get_params()['param_grid'][x]]
+        y_values = [score_trans(v, yfunc) for v in gs_model.get_params()['param_grid'][y]]
+        cv_score_df = cv_score_df.loc[y_values, x_values]
+        # plot
+        sns.heatmap(cv_score_df, annot=True)
+
+
+# --------------------------------------------------------------------------------
+# 数据展示模块
 # --------------------------------------------------------------------------------
 def plot_data_2D(X, y, method='pca'):
     """将数据降维 以2D的形式展现
@@ -297,3 +295,20 @@ def plot_data_2D(X, y, method='pca'):
     
     for i, iclass in enumerate(np.unique(y)):
         plt.scatter(x1[y==iclass], x2[y==iclass], alpha=0.5)
+
+def plot_multiclass_feature_dist(x, y, label_dict={}):
+    """展示各类的数据分布
+    Args
+        x: array
+            展示数据
+        y: array
+            label
+        label_dict: dict
+            legend展示的字典
+    """
+    classes = y.unique()
+    classes.sort()
+    for c in classes:
+        sns.distplot(x[y == c], label=str(label_dict.get(c, c)))
+    plt.legend()
+
