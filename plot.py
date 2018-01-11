92aa988f4953dc4522df7c31141225576b278806
zhanjiefan <809428197@qq.com>
2018-01-11 15:50:01 +0800

    修改plot.plot_confusion_matrix

--- plot.py
+++ plot.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+import itertools
+
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -142,23 +144,42 @@ def radar_plot(data, varlabels=None, legends=None, ax=None, class_size=None):
         ax.legend(legends)
 
 
-def plot_confusion_matrix(cm, classes=None):
-    """plot confusion_matrix
-
-    Args:
-        cm: array_like(2D)
-            confusion matrix
-        classes: array_like, default None
-            classes name
-
+# --------------------------------------------------------------------------------
+# 绘制confusion_matrix(copy from sklearn example)
+# --------------------------------------------------------------------------------
+def plot_confusion_matrix(cm, classes=None,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
     """
-    sns.heatmap(cm, annot=True, cmap=plt.cm.Blues, fmt='g', edgecolor='black')
-    if classes:
-        tick_marks = np.arange(len(classes)) + 0.5
-        plt.xticks(tick_marks, classes)
-        plt.yticks(tick_marks, classes)
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if classes is None:
+        classes = list(range(cm.shape[0]))
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
+
 
 # --------------------------------------------------------------------------------
 # 绘制PR_curve
